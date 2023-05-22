8a8e3fea8c77d42315ab815c1652933ef125a959
528982tts <mmuzammiloff@gmail.com>
2023-05-22 17:06:52 +0500

    updated: 17:06 2/05/2023

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,11 @@ import Header from "@/components/layout/header/page";
 import Logo from '@/components/layout/logo/page'
 import Navbar from '@/components/layout/Navbar/page'
 import Footer from '@/components/layout/Footer/page'
-import { ErrorBoundary } from "react-error-boundary";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-      <ErrorBoundary fallback={<div>Something went wrong</div>}>
         <Providers>
           <Header />
           <Logo />
@@ -20,7 +18,6 @@ export default function layout({ children }: { children: React.ReactNode }) {
           {children}
           <Footer />
         </Providers>
-        </ErrorBoundary>
       </body>
     </html>
   );
